c31351bd2c9fbfb6934057d1f6cee69c4bc72ddb
MohammadMehdis <your-email@example.com>
2024-09-18 00:59:35 +0330

    Finish The main page (Ui)

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,15 @@ module.exports = {
         estedadBold: ["Estedad-Bold"],
         estedadRegular: ["Estedad-Regular"],
         estedadMedium: ["Estedad-Medium"],
+        estedadSemiBold: ["Estedad-SemiBold"],
       },
       colors: {
         black: "#353535",
-        white: "#FFFFFF",
+        white: {
+          100: "#FFFFFF",
+          200: "#F9F9F9",
+          300: "#EDEDED",
+        },
         green: {
           100: "#417F56",
           200: "#315F41",
@@ -20,6 +25,12 @@ module.exports = {
           200: "#E5F2E9",
           300: "#CBCBCB",
         },
+      },
+      boxShadow: {
+        custom:
+          "0px 16px 6px rgba(0, 0, 0, 0.01), 0px 9px 5px rgba(0, 0, 0, 0.05), 0px 4px 4px rgba(0, 0, 0, 0.09), 0px 1px 2px rgba(0, 0, 0, 0.1), 0px 0px 0px rgba(0, 0, 0, 0.1)",
+        custom2:
+          " 0px 16px 6px rgba(0, 0, 0, 0.01), 0px 9px 5px rgba(0, 0, 0, 0.05), 0px 4px 4px rgba(0, 0, 0, 0.09), 0px 1px 2px rgba(0, 0, 0, 0.1), 0px 0px 0px rgba(0, 0, 0, 0.1)",
       },
     },
   },
